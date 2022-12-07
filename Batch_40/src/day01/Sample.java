8a3143afeb747e070cdc4b05e50ff5f5b21c69c7
Hp <Hp@DESKTOP-MEHK842>
2022-12-07 16:07:38 +0530

    Second Commit on Test Repository

--- Batch_40/src/day01/Sample.java
+++ Batch_40/src/day01/Sample.java
@@ -5,7 +5,7 @@ public class Sample
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome all  the 26 students from Batch 40.");
+		System.out.println("Please Submit your Assignments");
 
 	}
 
